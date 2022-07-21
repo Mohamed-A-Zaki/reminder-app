3d047f269d638d28235e37b1db1907bc602d34e6
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2022-07-21 02:51:22 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  // </React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
